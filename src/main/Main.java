862e9e02fb2e067187a57120fa433243aea31bd2
vlada <vladavr3@gmail.com>
2022-03-30 22:19:34 +0200

    update project

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class Main {
 
@@ -12,24 +14,51 @@ public class Main {
 
         System.setProperty("webdriver.chrome.driver", "src/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();//otvara browser
-        Thread.sleep(2000);
         driver.manage().window().maximize();
-        Thread.sleep(2000);
         String url = "https://the-internet.herokuapp.com/login";
         driver.get(url);
-        Thread.sleep(1500);
         WebElement username = driver.findElement(By.cssSelector("input[name='username']"));
         username.sendKeys("tomsmith");
 
-        //driver.findElement(By.id("username")) bez #
-        Thread.sleep(1500);
-
         driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
-        Thread.sleep(1500);
         driver.findElement(By.className("radius")).click();
-        //uhvatiti lokator sa tekstom nakon logovanja i ispisati tekst u konzoli
+
+        WebElement loggedInText = driver.findElement(By.id("flash"));
+//        System.out.println("Text u elementu: " + loggedInText.getText());
+//        System.out.println("Boja elementa: " + loggedInText.getCssValue("background-color"));
+
+        String axtualBackgroundColor = loggedInText.getCssValue("background-color");
+
+        String expectedText = "You logged into a secure area!";
+        String expectedColor = "rgba(93, 164, 35, 1)";
+
+        String actual[] = loggedInText.getText().split("(?<=!)"); //regex
+//        System.out.println("actual prvi " + actual[0]);
+//        System.out.println("actual drugi" + actual[1]);
+
+        String actualtext = actual[0];
+        String actualText2 = loggedInText.getText().substring(0, loggedInText.getText().length()-1);
+        //System.out.println("Actual 2: " + actualText2);
+        Thread.sleep(3000);
+
+        boolean result = expectedText.equals(actualtext);
+
+        Assert.assertTrue(result);
+
+        Assert.assertEquals(actualtext, expectedText);
+        Assert.assertEquals(axtualBackgroundColor, expectedColor);
+
+        driver.quit();
+
+        //runner
 
     }
+
+    @Test
+    public void test(){
+        System.out.println("Hello from test");
+    }
+
 
 
 
---
+++ src/test/TestClass.java
@@ -0,0 +1,37 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+
+    @Test
+    public void test() {
+        System.out.println("Hello from test");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("Hello from test");
+    }
+
+    @Test
+    public void test3() {
+        System.out.println("Hello from test");
+    }
+
+    @Test
+    public void test4() {
+        System.out.println("Hello from test");
+    }
+
+    @Test
+    public void test5() {
+        System.out.println("Hello from test");
+    }
+
+    @Test
+    public void test6() {
+        System.out.println("Hello from test");
+    }
+}
